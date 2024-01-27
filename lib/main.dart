9bc48f4bad384652bfd74786b102ec1f6bc97f8d
Markandey Pathak <markandeypathak@live.com>
2024-01-27 05:04:13 +0000

    :coffin: Removed unused import

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:checkmate/services/user_service.dart';
 import 'package:checkmate/splash.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:realm/realm.dart';
 
